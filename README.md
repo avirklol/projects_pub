4777beba103528049031c3b044287c5ad645b180
Annand Virk <annandvirk@gmail.com>
2024-02-03 16:13:26 -0500

    Modified README.md and created two new folders to support the other projects.

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ A growing list of projects that I'm happy to receive feedback on.
 ### The Intelligent Debtor
 A simple tool to set a budget and provide insight into the best way to pay off purchases on a credit card; you set the amount you're willing to pay in interest and it'll give you the best payment plan according to your interest rates.
 
-Good debt is the best way to grow your credit score!
+### Basic Stock Price Predictor
+As easy as basic as it gets; you plug in a stock symbol and this thing should predict the price; we all know it won't be accurate.
+
+### Simple Text Adventure
+A quick little text adventure game featuring a prisoner (you) and a guard (conversational AI, built off of an LLM).
--- intelligent_debtor/web_app.py
+++ intelligent_debtor/web_app.py
@@ -2,18 +2,40 @@
 import taipy as tp
 from taipy.gui import Html as HTML
 import numpy as np
+import pandas as pd
 
-# APP INSTANTIATION
+#WEB APP
+path_to_csv = 'dataset.csv'
 
+def get_data(path_to_csv: str):
+    # pandas.read_csv() returns a pd.DataFrame
+    dataset = pd.read_csv(path_to_csv)
+    dataset["Date"] = pd.to_datetime(dataset["Date"])
+    return dataset
 
-root_html = "<p>Multi-page application</p>"
-page1_html = "<h2>This is page 1</h2>"
-page2_html = "<h2>This is page 2</h2>"
+# Read the dataframe
 
-pages = {
-    "/": HTML(root_html),
-    "page1": HTML(page1_html),
-    "page2": HTML(page2_html)
-}
+dataset = get_data(path_to_csv)
 
-tp.Gui(pages=pages, css_file='intelligent_debtor/styles.css').run()
+# Initial value
+n_week = 10
+
+# Definition of the page
+page = """
+# Getting started with Taipy
+
+Week number: *<|{n_week}|>*
+
+Interact with this slider to change the week number:
+<|{n_week}|slider|min=1|max=52|>
+
+## Dataset:
+
+Display the last three months of data:
+<|{dataset[9000:]}|chart|type=bar|x=Date|y=Value|height=100%|>
+
+<|{dataset}|table|height=400px|width=95%|>
+"""
+
+# Create a Gui object with our page content
+tp.Gui(page=page).run(dark_mode=False)
