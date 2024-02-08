5d0de9787c7990ba3c85e6fe75ae6411e7d3efee
Eric Bright <ericbdev@gmail.com>
2024-02-08 08:41:05 -0500

    Use different langchain openai package

--- text_adventure/llm_playground/llm_app.py
+++ text_adventure/llm_playground/llm_app.py
@@ -1,14 +1,20 @@
 import streamlit as st
-from langchain_community.llms import openai
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
+
+load_dotenv()
 
 st.title(':rainbow[_Basic LLM_]')
 
 with st.sidebar:
-    openai_api_key = st.text_input('OpenAI API Key', type='password')
+    openai_api_key = st.text_input('OpenAI API Key', type='password', value=os.getenv("OPENAI_API_KEY"))
+
 
 def generate_response(input):
-    llm = openai(temperature=0.7, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input))
+
 
 with st.form('input_form', border=True):
     input = st.text_area('Your input:', 'Think of something to ask the LLM.')
