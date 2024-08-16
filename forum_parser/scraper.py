8fed48979e405126e156cca09ec2cb2dd53f2bc7
Annand Virk <annandv@unito.io>
2024-08-16 18:52:23 -0400

    Optimized scraper.py further; nested functions within run() so that parameters no longer need to be passed and the the functions can access all declared objected within scope.

--- forum_parser/scraper.py
+++ forum_parser/scraper.py
@@ -17,39 +17,7 @@ class ScraperObject:
         attrs = ',\n'.join(f"{key}={value}" for key, value in self.__dict__.items())
         return f"{self.__class__.__name__}(\n{attrs}\n)"
 
-def post_scrape(driver, posts: list, post_titles: list) -> list:
 
-    # The start of the following [for loop] slices [post_titles] using the length of the [posts] as the starting index, causing the [post_scrape] function to only
-    # pull post body content from new posts loaded on to a page that features endless scrolling.
-
-    # Will need to update this for paginated forums.
-
-    for post in post_titles[len(posts):]:
-        post_data = {}
-        post_data['title'] = post.find_element(By.TAG_NAME, 'a').get_attribute('title')
-        post_data['url'] = post.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        posts.append(post_data)
-
-    return posts
-
-def scroll_down_page(driver, scroll_pause_time=1, scroll_increment=400):
-
-    # Get the initial scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-
-    while True:
-        # Scroll down by a certain amount
-        driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_increment)
-
-        # Wait to load the page
-        time.sleep(scroll_pause_time)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            # If heights are the same, break the loop
-            break
-        last_height = new_height
 
 def run(scraper_object) -> list:
 
@@ -65,7 +33,10 @@ def run(scraper_object) -> list:
     # Load URL:
     driver.get(scraper_object.url)
 
-    # List to store dictionaries of posts {title, url, body}:
+    # List to store dictionaries of post title & URL data {title, url}:
+    post_titles = []
+
+    # List to store dictionaries of complete post data {title, url, body}:
     posts = []
 
     # Post Amount Target:
@@ -79,7 +50,63 @@ def run(scraper_object) -> list:
     # Input Related:
     repeated_input_type = scraper_object.repeated_input_type
     repeated_click_xpath = scraper_object.repeated_click_xpath
+    paginated = scraper_object.paginated
     actions = scraper_object.actions
+
+    # NESTED FUNCTIONS BEGIN
+
+    # Sroll Page Function:
+    def scroll_down_page(scroll_pause_time=1, scroll_increment=400):
+
+        # Get the initial scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down by a certain amount
+            driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_increment)
+
+            # Wait to load the page
+            time.sleep(scroll_pause_time)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                # If heights are the same, break the loop
+                break
+            last_height = new_height
+    
+    # Post Title & URL Scrape Function:
+    def post_scrape() -> list:
+
+        nonlocal post_titles
+
+        if avoid_class != None:
+            post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class) if avoid_class not in post.get_attribute('class')]
+        else:
+            post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class)]
+
+        def scrape(post_titles):
+            
+            nonlocal posts
+
+            for post in post_titles:
+                post_data = {}
+                title = post.find_element(By.TAG_NAME, 'a').get_attribute('title')
+                if title != '':
+                    post_data['title'] = title
+                else:
+                    post_data['title'] = post.find_element(By.TAG_NAME, 'a').text
+                post_data['url'] = post.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                posts.append(post_data)
+                
+        if paginated:
+            scrape(post_titles)
+        else:
+            scrape(post_titles[len(posts):])
+
+        return posts
+    
+    # NESTED FUNCTIONS END
 
     # Intial Input Actions:
     for action_name, action_selector in actions.items():
@@ -100,37 +127,30 @@ def run(scraper_object) -> list:
         try:
             if repeated_input_type == 'Click':
                 try:
-                    if avoid_class != None:
-                        post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class) if avoid_class not in post.get_attribute('class')]
-                        posts = post_scrape(driver, posts, post_titles)
-                        if posts >= n:
-                            break
-                        else:
-                            input = wait.until(EC.element_to_be_clickable((By.XPATH, repeated_click_xpath)))
-                            input.click()
+                    posts = post_scrape()
+                    if len(posts) >= n:
+                        break
                     else:
-                        post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class)]
-                        posts = post_scrape(driver, posts, post_titles)
-                        if posts >= n:
-                            break
-                        else:
-                            input = wait.until(EC.element_to_be_clickable((By.XPATH, repeated_click_xpath)))
-                            input.click()
+                        input = wait.until(EC.element_to_be_clickable((By.XPATH, repeated_click_xpath)))
+                        input.click()
                 except Exception as e:
                     print(f'Error: {e}')
+
+            # The following block will definitely need work.
+
             else:
                 try:
                     if avoid_class != None:
                         post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class) if avoid_class not in post.get_attribute('class')]
-                        posts = post_scrape(driver, posts, post_titles)
+                        posts = post_scrape(posts, post_titles)
                         if len(posts) >= n:
                             break
                         else:
-                            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
                     else:
                         post_titles = [post for post in driver.find_elements(By.CLASS_NAME, post_title_class)]
-                        posts = post_scrape(driver, posts, post_titles)
-                        if posts >= n:
+                        posts = post_scrape(posts, post_titles)
+                        if len(posts) >= n:
                             break
                         else:
                             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
